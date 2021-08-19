98c2302fd7dee6b08c47b5295f8e51d78ea1d29f
arfranzim <andrefranzim@gmail.com>
2021-08-19 13:36:23 -0300

    Estrutura Inicial

---
+++ src/main/java/br/com/franzim/ciss/CissTestApplication.java
@@ -0,0 +1,13 @@
+package br.com.franzim.ciss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CissTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CissTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/franzim/ciss/CissTestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.franzim.ciss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CissTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
